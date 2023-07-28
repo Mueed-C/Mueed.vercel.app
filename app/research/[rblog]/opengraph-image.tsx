66a1852c020db0ef036526d5f45cc06aa7d6cb3b
Mueed-C <wmcmediabs@gmail.com>
2023-07-28 20:23:18 +0500

    not working for some reason, attempt at fix.

--- app/research/[rblog]/opengraph-image.tsx
+++ app/research/[rblog]/opengraph-image.tsx
@@ -1,9 +1,5 @@
-import { getrBlog } from "@/sanity/sanity.utils";
 import { ImageResponse } from "next/server";
-import { NextRequest } from "next/server";
-type ImageProps = {
-  params: { rblog: string }; // Use the same type as in your page.tsx
-};
+
 // Route segment config
 export const runtime = "edge";
 
@@ -16,11 +12,6 @@ export const size = {
 
 export const contentType = "image/png";
 
-// // Font
-// const interSemiBold = fetch(
-//   new URL("./Inter-SemiBold.ttf", import.meta.url)
-// ).then((res) => res.arrayBuffer());
-
 // Image generation
 export default async function Image() {
   return new ImageResponse(
@@ -28,53 +19,16 @@ export default async function Image() {
       // ImageResponse JSX element
       <div
         style={{
-          display: "flex",
-          height: "100%",
+          fontSize: 128,
+          background: "white",
           width: "100%",
+          height: "100%",
+          display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          flexDirection: "column",
-          backgroundImage: "linear-gradient(to bottom, #dbf4ff, #fff1f1)",
-          fontSize: 60,
-          letterSpacing: -2,
-          fontWeight: 700,
-          textAlign: "center",
         }}
       >
-        <div
-          style={{
-            backgroundImage:
-              "linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))",
-            backgroundClip: "text",
-            "-webkit-background-clip" as any: "text",
-            color: "transparent",
-            margin: 20,
-          }}
-        >
-          My Research, in detail!
-        </div>
-        <div
-          style={{
-            backgroundImage:
-              "linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))",
-            backgroundClip: "text",
-            "-webkit-background-clip": "text",
-            color: "transparent",
-          }}
-        ></div>
-
-        <div
-          style={{
-            backgroundImage:
-              "linear-gradient(90deg, rgb(255, 77, 77), rgb(249, 203, 40))",
-            backgroundClip: "text",
-            "-webkit-background-clip": "text",
-            color: "transparent",
-            margin: 20,
-          }}
-        >
-          Mueed Chaudhary
-        </div>
+        My Research, in detail!
       </div>
     ),
     // ImageResponse options
@@ -82,14 +36,6 @@ export default async function Image() {
       // For convenience, we can re-use the exported opengraph-image
       // size config to also set the ImageResponse's width and height.
       ...size,
-      // fonts: [
-      //   {
-      //     name: "Inter",
-      //     data: await interSemiBold,
-      //     style: "normal",
-      //     weight: 400,
-      //   },
-      // ],
     }
   );
 }
