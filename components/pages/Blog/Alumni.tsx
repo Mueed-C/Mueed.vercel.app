1925ad635e70dba479014f88ca672305e2f45c9e
Mueed-C <wmcmediabs@gmail.com>
2023-07-31 06:43:27 +0500

    fix...

--- components/pages/Blog/Alumni.tsx
+++ components/pages/Blog/Alumni.tsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import Section from '@/components/parts/Section'
-import Card from '@/components/parts/Card'
+import React from "react";
+import Section from "@/components/parts/Section";
+import Card from "@/components/parts/Card";
 const Alumni = () => {
   return (
-    <div id="blog_alumni" >
-    <Section title="Welcome, WMC Alumni!" mbot='4' isF>
-      <p className=' mt-7'>Over here I will share and discuss various resources that may be useful for you all. A lot of what I will share here is what I&#39;ve often been asked about by my classfellows, seniors, and juniors during my 5 years here in Pakistan, so I know it will prove useful to some of you :).
-      </p>
-      <div id="tag_filters"  >
-        {/* Display all tags as options here:
+    <div id="blog_alumni">
+      <Section title="Welcome, WMC Alumni!" mbot="4" isF>
+        <p className=" mt-7">
+          Over here I will share and discuss various resources that may be
+          useful for you all. A lot of what I will share here is what I&#39;ve
+          often been asked about by my classfellows, seniors, and juniors during
+          my 5 years here in Pakistan, so I know it will prove useful to some of
+          you :).
+        </p>
+        <div id="tag_filters">
+          {/* Display all tags as options here:
            I think, use a badge icon?*/}
-        {/* copied over from blog_research, but im
+          {/* copied over from blog_research, but im
         not sure what tags would be used here yet.. */}
+        </div>
+      </Section>
 
-      </div>    
-    </Section>
-
-    <Section>
+      <Section>
         <section className="grid gap-4 sm:grid-cols-2">
-  {/* <Card
+          {/* <Card
     imageUrl="https://t3.ftcdn.net/jpg/05/65/73/56/360_F_565735615_D2uX6alZuUOu77bJkfh5JKJbKmyZmxNu.jpg"
     heading="Card 1"
     description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum laudantium placeat sunt hic voluptatem reprehenderit? Labore commodi officia sit veritatis minus deleniti non consequatur earum magni, distinctio illo tempora nobis!Lorem ipsum dolor sit amet, consectetur adipiscing elit."
@@ -30,7 +34,7 @@ const Alumni = () => {
     description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."
   />
   {/* Add more Card components as needed */}
-  <Card
+          {/* <Card
     imageUrl="https://t3.ftcdn.net/jpg/05/65/73/56/360_F_565735615_D2uX6alZuUOu77bJkfh5JKJbKmyZmxNu.jpg"
     heading="Card 3"
     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
@@ -55,11 +59,10 @@ const Alumni = () => {
     heading="Card 7"
     description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."
   /> */}
-</section>
+        </section>
+      </Section>
+    </div>
+  );
+};
 
-        </Section>
-</div>
-  )
-}
-
-export default Alumni
+export default Alumni;
