f6fcfd7c8be64cf2dec50d5ad9d5ea3b0379ff2b
Mueed-C <wmcmediabs@gmail.com>
2024-01-16 08:23:12 +0500

    improvement based on feedback

--- components/parts/Card.tsx
+++ components/parts/Card.tsx
@@ -71,7 +71,7 @@ const Card: React.FC<CardProps> = ({
           </Badge>
         ) : (
           <Badge variant="outline" className="border-gray-500">
-            Upcoming... 
+            Upcoming...
           </Badge>
         )}
       </div>
